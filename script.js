4c236eb66ec44faa104bc6f4d271790cecfe1ca4
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-09-10 22:10:48 +0100

    feat: add functions to scripts and created episodes

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
 //You can edit ALL of the code here
-{/* <section id="card">
-      <article>
-        <h2>Movie name</h2>
-        <img src="" alt="">
-        <p id="season-episode">Season & Episodes</p>
-        <p id="summary">Summary</p>
-      </article>
-    </section> */}
+import episodeSelect from './selectMenu.js';
+import searchEpisode from './search.js';
 
-
-
-
+let allEpisodes = [];
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  episodeSelect(allEpisodes);
+
 }
 
 function makePageForEpisodes(episodeList) {
-  const section = document.createElement('section');
-  episodeList.forEach(movie => {
-    const article = document.createElement('article');
-    const image = document.createElement('img');
-    const h2 = document.createElement('h2');
-    const p = document.createElement('p');
+  const rootElem = document.getElementById("root");
+  rootElem.innerHTML = "";
+  const section = document.createElement("section");
+  section.className = "sectionEpisodes";
+  episodeList.forEach((movie) => {
+    const h2 = document.createElement("h2");
+    const article = document.createElement("article");
+    const image = document.createElement("img");
+    const duration = document.createElement('h3');
+    const p = document.createElement("p");
 
+    h2.innerText = `${movie.name} - S${("" + movie.season).padStart(2, "0")}E${(
+      "" + movie.number
+    ).padStart(2, "0")}`;
     image.src = movie.image.medium;
     image.alt = movie.name;
-    h2.innerText = `${movie.name} - S${(''+movie.season).padStart(2, '0')} ${(''+movie.number).padStart(2,'0')}`
-    p.innerText =movie.summary;
+    duration.innerText = durationFormat(movie.runtime);
+    function durationFormat (minutes) {
+      const min = ((minutes % 60)+'').padStart(2,0);
+      const hour = Math.floor(minutes / 60);
+      return `${hour}h${min}min`;
+      }
+    p.innerHTML = movie.summary;
 
-    article.appendChild(image);
     article.appendChild(h2);
+    article.appendChild(image);
+    article.appendChild(duration);
     article.appendChild(p);
     section.appendChild(article);
-
-  })
-  const rootElem = document.getElementById("root");
+  });
   rootElem.appendChild(section);
 }
 
 window.onload = setup;
+
+const search = document.getElementById("search");
+const displayNumberOfMovies = document.getElementById("displayNumberOfMovies");
+
+//search function called
+search.addEventListener("input", (event) => {
+   let searchEpisodes = searchEpisode(event.target.value, allEpisodes); // return u buraya store ediyor
+    displayNumberOfMovies.innerText = `Displaying ${searchEpisodes.length}/73 episodes`;
+  makePageForEpisodes(searchEpisodes);
+});
+
+//select function called
+ document.getElementById("episodeSelect").addEventListener("change", (event) => {
+   let selectedEpisode = searchEpisode(event.target.value, allEpisodes);
+   makePageForEpisodes(selectedEpisode);
+ });
