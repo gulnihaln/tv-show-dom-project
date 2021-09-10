18141f134642040e7bc874b64fb85ad223f930fc
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-09-10 22:09:54 +0100

    feat: add select function

---
+++ selectMenu.js
@@ -0,0 +1,19 @@
+// creates episode option menu content
+export default function episodeSelect(allEpisodes) {
+  const select = document.getElementById("episodeSelect");
+  var fragment = document.createDocumentFragment();
+
+  allEpisodes.forEach((episode) => {
+    const option = document.createElement("option");
+    option.text = `S${("" + episode.season).padStart(2, 0)}E${(
+      "" + episode.number
+    ).padStart(2, 0)} - ${episode.name}`;
+    option.value = episode.name;
+    fragment.appendChild(option);
+  });
+
+  select.appendChild(fragment);
+
+}
+
+
